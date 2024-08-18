2f21bc2f048d96ecad0f317aeb9e7a6db85593e8
Sonseongoh <nobleser@naver.com>
2024-08-19 01:29:18 +0900

    Setting: eslint setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,36 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier'; // prettier 플러그인 import
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+export default [
+	{
+		ignores: ['dist'],
+	},
+	{
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			parser: tsParser,
+			globals: globals.browser,
+		},
+		plugins: {
+			'@typescript-eslint': tseslint,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			prettier: prettier, // prettier 플러그인 추가
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...tseslint.configs.recommended.rules,
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+			'prettier/prettier': 'error', // prettier 규칙 적용
+		},
+	},
+];
