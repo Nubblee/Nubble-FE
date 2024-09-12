d61c3ac0aaa6504ed79133ad15d5688935e656ed
Sonseongoh <nobleser@naver.com>
2024-09-13 03:11:33 +0900

    라우팅 초기설정

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import { Outlet, useLocation } from 'react-router-dom'
+
+const Layout: React.FC = () => {
+	const location = useLocation()
+
+	// 헤더가 없는 경로 리스트 (예: 로그인, 회원가입 페이지)
+	const noHeaderRoutes = ['/login']
+
+	// 현재 경로가 noHeaderRoutes에 포함되는지 확인
+	const hideHeader = noHeaderRoutes.includes(location.pathname)
+
+	return (
+		<div>
+			{!hideHeader && <header>Header</header>} {/* 조건에 따라 헤더 표시 */}
+			<main>
+				<Outlet /> {/* 중첩된 라우트 렌더링 */}
+			</main>
+			<footer>Footer</footer>
+		</div>
+	)
+}
+
+export default Layout
