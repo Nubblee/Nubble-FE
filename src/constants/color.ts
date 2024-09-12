83d005894ff126baa982f64384fd48d2b45e7fe3
Sumin <tnalsdldkr2@naver.com>
2024-09-13 02:49:28 +0900

    feat/전역 스타일링 적용 (#4)

---
+++ src/constants/color.ts
@@ -0,0 +1,12 @@
+const colors = {
+	primaryBlue: '#ACC1EB',
+	mainBlack: '#0C0C0C',
+	bgBlack: '#1F1F1F',
+	mainGray: '#3D3D3D',
+	commentBlack: '#434343',
+	deleteGray: '#858585',
+	commentGray: '#9f9f9f',
+	white: '#FFFFFF',
+}
+
+export default colors
---
+++ src/constants/font.ts
@@ -0,0 +1,23 @@
+export const fontSize = {
+	xxxxl: '32px',
+	xxxl: '28px',
+	xxl: '24px',
+	xl: '20px',
+	lg: '18px',
+	md: '16px',
+	sm: '14px',
+	xs: '12px',
+	xxs: '11px',
+}
+
+export const fontWeight = {
+	thin: 100,
+	extraLight: 200,
+	light: 300,
+	regular: 400,
+	medium: 500,
+	semiBold: 600,
+	bold: 700,
+	extraBold: 800,
+	black: 900,
+}
