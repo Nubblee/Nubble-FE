1e0504d0d3cc5e4fb90411e86545525c64183d93
Sonseongoh <nobleser@naver.com>
2024-09-13 18:58:42 +0900

    design/플로팅메뉴(좋아요,공유) 디자인 (#1)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import FloatingMenu from '@components/FloatingMenu'
 import React from 'react'
 
 const Home: React.FC = () => {
-	return <h1>Welcome to the Home Page</h1>
+	return (
+		<>
+			<FloatingMenu />
+			<h1>Welcome to the Home Page</h1>
+		</>
+	)
 }
 
 export default Home
