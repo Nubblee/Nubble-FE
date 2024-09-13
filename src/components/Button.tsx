ff58f1f6dfa10b6b26954cba499bd0c78ddcd2a2
jizero <jizero27@gmail.com>
2024-09-13 13:32:55 +0900

    design/buttonComponent 버튼 컴포넌트 구현 (#2)

---
+++ src/components/Button.tsx
@@ -0,0 +1,85 @@
+import styled from '@emotion/styled'
+import React, { ReactNode } from 'react'
+import colors from '../constants/color'
+import { fontSize, fontWeight } from '../constants/font'
+import { Link } from 'react-router-dom'
+
+type ButtonVariant = 'primary' | 'secondary'
+type ButtonSize = 'sm' | 'md'
+
+export interface ButtonProps {
+	children: ReactNode
+	variant?: ButtonVariant
+	size?: ButtonSize
+	radius?: number
+	disabled?: boolean
+	to?: string
+	onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button = ({
+	children,
+	variant = 'primary',
+	size = 'md',
+	radius = 10,
+	disabled = false,
+	to,
+	onClick,
+}: ButtonProps) => {
+	return to && !disabled ? (
+		<Link to={to}>
+			<ButtonComponent variant={variant} radius={radius} disabled={disabled} size={size}>
+				{children}
+			</ButtonComponent>
+		</Link>
+	) : (
+		<ButtonComponent
+			variant={variant}
+			onClick={onClick}
+			radius={radius}
+			disabled={disabled}
+			size={size}
+		>
+			{children}
+		</ButtonComponent>
+	)
+}
+
+export default Button
+
+const sizeStyles = {
+	sm: {
+		width: '65px',
+		height: '29px',
+		padding: '5px',
+		fontSize: `${fontSize.sm}`,
+	},
+	md: {
+		width: '90px',
+		height: '37px',
+		padding: '8px',
+		fontSize: `${fontSize.md}`,
+	},
+}
+const ButtonComponent = styled.button<ButtonProps>`
+	width: ${({ size = 'md' }) => sizeStyles[size].width};
+	height: ${({ size = 'md' }) => sizeStyles[size].height};
+	font-size: ${({ size = 'md' }) => sizeStyles[size].fontSize};
+	padding: ${({ size = 'md' }) => sizeStyles[size].padding};
+	font-weight: ${fontWeight.medium};
+	align-items: center;
+	justify-content: center;
+	border-radius: ${({ radius }) => radius}px;
+	background-color: ${({ variant }) =>
+		variant === 'primary' ? `${colors.primaryBlue}` : 'transparent'};
+	color: ${({ variant }) =>
+		variant === 'primary' ? `${colors.mainGray}` : `${colors.primaryBlue}`};
+
+	&:hover {
+		cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+		background-color: ${({ variant }) =>
+			variant === 'primary' ? `${colors.hoverBlue}` : `${colors.deleteGray}`};
+		color: ${({ variant }) => variant === 'primary' && `${colors.white}`};
+		transition: 0.2s;
+	}
+`
