06d56fa6b92c8e2c5bd8ecc21a9a524921a90706
Sonseongoh <nobleser@naver.com>
2024-09-24 14:49:02 +0900

    help/날짜만 옆으로 맞춰서 정렬하고 싶어요 (#59)

--- src/pages/AddQuestion.tsx
+++ src/pages/AddQuestion.tsx
@@ -1,17 +1,21 @@
 import styled from '@emotion/styled'
 import colors from '@/constants/color'
 import Button from '@components/Button'
+import { fontSize } from '@/constants/font'
 
 const AddQuestion = () => {
 	return (
 		<Container>
 			<Form>
+				<Title>코딩테스트 문제 추가하기</Title>
 				<InputZone>
 					<InputDate type="date" placeholder="날짜 입력" />
 					<Input placeholder="문제 제목을 입력해주세요" />
 					<Input placeholder="문제 링크를 입력해주세요" />
 				</InputZone>
-				<SubmitButton>등록하기</SubmitButton>
+				<ButtonWrapper>
+					<SubmitButton>등록하기</SubmitButton>
+				</ButtonWrapper>
 			</Form>
 			<QuestionList>{/* 문제 리스트 부분 */}</QuestionList>
 		</Container>
@@ -26,7 +30,7 @@ const Container = styled.div`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	margin: 0 auto; /* 중앙 정렬 */
+	margin: 0 auto;
 	padding: 20px;
 `
 
@@ -34,13 +38,20 @@ const Form = styled.form`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-	align-items: center;
+
 	background-color: ${colors.mainGray};
 	gap: 20px;
 	width: 100%;
-	height: 300px;
-	padding: 20px;
+	height: 320px;
+	padding: 10px;
 	border-radius: 8px;
+`
+
+const Title = styled.h1`
+	display: flex;
+	justify-content: center;
+	font-size: ${fontSize.xxxl};
+	margin-top: 30px;
 `
 
 const InputZone = styled.div`
@@ -48,24 +59,23 @@ const InputZone = styled.div`
 	flex-direction: column;
 	align-items: center;
 	width: 100%;
-	gap: 10px;
+	gap: 14px;
 `
 
 const InputDate = styled.input`
 	width: 100%;
-	max-width: 300px;
+	max-width: 160px;
 	height: 40px;
 	padding: 8px;
 	font-size: 16px;
 	border-radius: 4px;
-
 	background-color: ${colors.commentGray};
 	color: white;
 `
 
 const Input = styled.input`
 	width: 100%;
-	max-width: 600px;
+	max-width: 500px;
 	height: 40px;
 	padding: 8px;
 	font-size: 16px;
@@ -75,10 +85,15 @@ const Input = styled.input`
 	color: white;
 `
 
+const ButtonWrapper = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	margin: 0 10px 18px 0;
+`
+
 const SubmitButton = styled(Button)`
 	width: 100px;
 	height: 40px;
-
 	color: white;
 	font-size: 16px;
 `
