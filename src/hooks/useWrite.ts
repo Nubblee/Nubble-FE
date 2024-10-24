ec5382a9f758ce0afb07311a484a22361cdf45a5
jizero <jizero27@gmail.com>
2024-10-24 14:22:27 +0900

    fix: 카테고리, 서브카테고리 상태값 저장, useCategory 생성 #91

--- src/hooks/useWrite.ts
+++ src/hooks/useWrite.ts
@@ -3,14 +3,16 @@ import { useWriteStore } from '@/stores/writeStore'
 export default function useWrite() {
 	const markdownContent = useWriteStore((state) => state.content)
 	const markdownTitle = useWriteStore((state) => state.title)
-	const categories = useWriteStore((state) => state.category)
+	const category = useWriteStore((state) => state.category)
+	const board = useWriteStore((state) => state.board)
 	const boardId = useWriteStore((state) => state.boardId)
 	const thumbnail = useWriteStore((state) => state.thumbnailImg)
 	const description = useWriteStore((state) => state.description)
 	const setTitle = useWriteStore((state) => state.setTitle)
 	const setContent = useWriteStore((state) => state.setContent)
 	const setThumbnail = useWriteStore((state) => state.setThumbnail)
-	const setCategories = useWriteStore((state) => state.setCategory)
+	const setCategory = useWriteStore((state) => state.setCategory)
+	const setBoard = useWriteStore((state) => state.setBoard)
 	const setBoardId = useWriteStore((state) => state.setBoardId)
 	const setDescription = useWriteStore((state) => state.setDescription)
 
@@ -19,21 +21,24 @@ export default function useWrite() {
 		setContent('')
 		setThumbnail('')
 		setDescription('')
-		setCategories([])
+		setCategory('')
+		setBoard('')
 		setBoardId(0)
 	}
 
 	return {
 		markdownContent,
 		markdownTitle,
-		categories,
+		category,
 		thumbnail,
+		board,
 		boardId,
 		description,
 		setTitle,
 		setContent,
 		setThumbnail,
-		setCategories,
+		setCategory,
+		setBoard,
 		setBoardId,
 		setDescription,
 		reset,
